99b9736d8992b8a28fb3bb4d8382bc1c7dac27cc
Manit Factory <106180627+Kokonattp@users.noreply.github.com>
2026-01-06 10:53:49 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     unoptimized: true,
   },
   // ถ้า repo ชื่อ vacation-leave ให้ใส่ basePath
-  // basePath: '/vacation-leave',
+   basePath: '/vacation-leave',
 }
 
 module.exports = nextConfig
